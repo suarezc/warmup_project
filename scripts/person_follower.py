df382e7d0a0c1f784a56d476dffe61ddabd0899c
suarezc <charlessuarez2000@gmail.com>
2021-04-11 12:49:51 -0500

    personfollower code donish

---
+++ scripts/person_follower.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from sensor_msgs.msg import LaserScan
+
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+distance = 0.75
+
+class PersonFollower(object):
+
+    def __init__(self):
+        rospy.init_node("person_follower")
+
+        rospy.Subscriber("/scan", LaserScan, self.process_scan)
+
+        self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
+        lin = Vector3()
+        ang = Vector3()
+        self.twist = Twist(linear=lin,angular=ang)
+
+    def process_scan(self,data):
+        angle_to_face = data.ranges.index(min(data.ranges))
+
+        #turn left if it will be closer
+        if angle_to_face > 180:
+            angle_to_face = angle_to_face - 360
+        kp = 0.5
+        diff = ((0 - angle_to_face)/180) * 3.14
+        angular = diff * kp
+
+    
+        self.twist.angular.z = -angular
+
+        #only move if the robot is far away enough and if it is facing the object within reason
+        if min(data.ranges) >= distance and (angle_to_face > -30 and angle_to_face < 30):
+            # Go forward if not close enough to wall.
+            self.twist.linear.x = 0.3
+        else:
+            # Close enough to wall, stop.
+            self.twist.linear.x = 0
+        # Publish msg to cmd_vel.
+        self.twist_pub.publish(self.twist)
+
+    def run(self):
+        # Keep the program alive.
+        rospy.spin()
+
+if __name__ == '__main__':
+    # Declare a node and run it.
+    node = PersonFollower()
+    node.run()
